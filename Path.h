97959cac0a9f1290b40212c5111a464516bd7e75
Yuuichi2010 <130419751+Yuuichi2010@users.noreply.github.com>
2024-12-27 06:26:23 +0700

    Add files via upload

--- Path.h
+++ Path.h
@@ -9,12 +9,15 @@ class ClassPath : public Shape {
 public:
     ClassPath(const std::string& pathData, const Transform& transform);
     void Draw(Graphics& graphics, std::vector<Defs*>& defs) override;
+    void convertPathToValue();
 
 private:
-    void convertPathToValue();
     std::string pathData;
     std::vector<float> values;
     std::vector<char> types;
+
+    void AddArc(PointF& lastPoint, float rx, float ry, float xAxisRotation, bool largeArcFlag, bool sweepFlag, PointF endPoint);
+    void AddArc(float cx, float cy, float rx, float ry, float startAngle, float deltaAngle, bool sweepFlag);
 };
 
 #endif // PATH_H
