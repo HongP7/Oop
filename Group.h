872130222b82590d15558720bbaa88a9a31f9236
Yuuichi2010 <130419751+Yuuichi2010@users.noreply.github.com>
2024-12-31 23:34:55 +0700

    Add files via upload

--- Group.h
+++ Group.h
@@ -1,21 +1,16 @@
 #ifndef GROUP_H
 #define GROUP_H
-
 #include "Shapes.h"
-#include <vector>
 
-class Group_ : public Shape
+class Group : public Shape
 {
 private:
     vector<Shape*> shapes;
     int fontSize;
 
 public:
-    Group_(vector<Shape*>&, float, float, RGB, RGB, float, Transform, int, string, string);
+    Group(vector<Shape*>&, float, float, RGB, RGB, float, Transform, int, string, string);
     void Draw(Graphics&, vector<Defs*>&) override;
-    void AddShape(Shape* shape);
-
 };
 
-
-#endif // GROUP_H
+#endif // !GROUP_H
