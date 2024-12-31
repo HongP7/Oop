633c15b3c37388557ce3a02addc57a7b5b2fa2ef
Yuuichi2010 <130419751+Yuuichi2010@users.noreply.github.com>
2024-12-31 23:35:30 +0700

    Add files via upload

--- Path.h
+++ Path.h
@@ -1,55 +1,23 @@
 ﻿#ifndef PATH_H
 #define PATH_H
-
+#define _USE_MATH_DEFINES
 #include "Shapes.h"
-#include "Defs.h"
-#include <vector>
-#include <string>
 
-struct Transform {
-    float scaleX, scaleY;
-    float translateX, translateY;
-    float rotateAngle;
-    float skewX, skewY;
-    std::vector<std::string> transformOrder;
+struct Path
+{
+    vector<float> value;
+    vector<char> type;
 };
 
-class Path {
-public:
-    std::vector<float> values;
-    std::vector<char> types;
-};
-
-class ClassPath : public Shape {
-public:
-    ClassPath(const std::string& pathData, const Transform& transform);
-    ClassPath(float fillOpacity, float strokeOpacity, float strokeWidth, RGB fillRGB, RGB strokeRGB, Transform transform, Path path, const std::string& fill, const std::string& stroke, float strokeDasharray, float strokeDashoffset);
-    void Draw(Graphics& graphics, std::vector<Defs*>& defs) override;
-    void convertPathToValue();
-    void addValue(float value);
-    void addType(char type);
-    void clear();
-
+class ClassPath : public Shape
+{
 private:
-    std::string pathData;
-    std::vector<float> values;
-    std::vector<char> types;
     Path path;
-    float fillOpacity;
-    float strokeOpacity;
-    float strokeWidth;
-    RGB fillRGB;
-    RGB strokeRGB;
-    Transform transform;
-    std::string fill;
-    std::string stroke;
-    float strokeDasharray;
-    float strokeDashoffset;
+    float width_out, height_out;
 
-    void AddArc(GraphicsPath& pathToDraw, PointF& lastPoint, float rx, float ry, float xAxisRotation, bool largeArcFlag, bool sweepFlag, PointF endPoint);
-    void AddArc(GraphicsPath& pathToDraw, float cx, float cy, float rx, float ry, float startAngle, float deltaAngle, bool sweepFlag);
+public:
+    ClassPath(float, float, float, RGB, RGB, Transform, Path, string, string, float, float);
+    void Draw(Graphics&, vector<Defs*>&) override;
 };
 
-void convertPathToValue(const std::string& pathData, Path& path);
-
-#endif // PATH_H
+#endif // !PATH_H
